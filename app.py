1b4f11ed27d2f4b191d9359b6f0412f6d699e423
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-12-16 10:49:22 +0530

    new client vertical done

--- app.py
+++ app.py
@@ -223,6 +223,38 @@ def industries():
     except Exception as e:
         logging.error(f"Error in processing request: {e}")
         return jsonify({"message": "Internal server error.", "status": "error"}), 500
+    
+
+# this API is responsible for selecting verticals
+@app.route("/chatbot/new_client/user_details/industries/verticals", methods=["POST"])
+def verticals_new_client():
+    try:
+        verticals = {
+            "1": "ML/DS/AI",
+            "2": "Sales force",
+            "3": "Microsoft dynamics",
+            "4": "Custom app",
+            "5": "Others",
+        }
+
+        data = request.get_json()
+        row_id = data.get("row_id")  # Get the user ID from the request
+
+        selected_options = data.get("selected_options", [])
+        selected_verticals = [verticals[opt] for opt in selected_options if opt in verticals]
+
+        vertical_str = ",".join(selected_verticals)
+
+        query = "UPDATE new_client SET VERTICAL = %s WHERE ID = %s"
+        values = (vertical_str, row_id)
+        cursor.execute(query, values)
+        mydb.commit()
+        logging.info("new client vertical saved in DB - {vertical_str}")
+        return jsonify({"selected_verticals": selected_verticals, "code": 200})
+    except Exception as e:
+        logging.error(f"Error in processing request: {e}")
+        return jsonify({"message": "Internal server error.", "status": "error"}), 500
+
 
 
 
