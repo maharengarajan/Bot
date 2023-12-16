fceeda881dc39e757581031e07349cb6de577596
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-12-16 12:39:49 +0530

    extract new client details code done

--- src/database.py
+++ src/database.py
@@ -11,6 +11,13 @@ def configure():
     load_dotenv()
 
 
+configure()
+host = os.getenv("database_host_name")
+user_name = os.getenv("database_user_name")
+password = os.getenv("database_user_password")
+database = os.getenv("database_name")
+
+
 def create_database():
     try:
 
@@ -50,7 +57,67 @@ def create_database():
         raise CustomException(e,sys)
     
 
+def extract_new_client_details():
+    try:
+
+        # Connection from Python to MySQL
+        mydb = conn.connect(host=host,user=user_name,password=password,database=database)
+
+        # Creating a pointer to the MySQL database
+        cursor = mydb.cursor()
+
+        # execute sql query to retrive new_client details
+        query = "SELECT * FROM new_client ORDER BY id DESC LIMIT 1"  # we can get the row with highest id value
+        cursor.execute(query)
+
+        # Fetch the result
+        result = cursor.fetchone()  # getting only one row
+
+        if result:
+            # Extract the columns from the result
+            (
+                id,
+                date,
+                time,
+                ip_address,
+                name,
+                email,
+                contact,
+                company,
+                selected_industry,
+                selected_vertical,
+                requirement,
+                known_source,
+            ) = result
+
+            # Extracted new_client details stored in dictionary format
+            new_client_details = {
+                "id": id,
+                "date": date,
+                "time": time,
+                "ip_address":ip_address,
+                "name": name,
+                "email": email,
+                "contact": contact,
+                "company": company,          
+                "industries_choosen": selected_industry,
+                "verticals_choosen": selected_vertical,
+                "requirement": requirement,
+                "known_source": known_source,
+            }
+
+            return new_client_details
+        
+        # Close the cursor and connection
+        cursor.close()
+        mydb.close()
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise CustomException(e,sys)
 
     
 
-create_database()
+    
+
+
+    
